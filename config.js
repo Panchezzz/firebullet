740cba08d8f3f983c40e12473c9dc663486cfc25
Panchezzz <cheesegamingstudio@gmail.com>
2025-06-26 22:31:30 +0500

    s

--- config.js
+++ config.js
@@ -3,20 +3,15 @@ module.exports = {
 	// Host address for the server
 	server_host: "0.0.0.0",
 
-	// Server port (по умолчанию лучше оставить 3000 или 443, если с SSL)
 	server_port: 3000,
 
-	// SSL отключен
 	ssl: false,
-	ssl_key: "",		// отключено
-	ssl_cert: "",		// отключено
-
-	// Описание сервера
+	ssl_key: "",		
+	ssl_cert: "",		
 	server_name: "HecateServer",
 	server_operator: "Hecate",
 	server_motd: "Hecate is online!",
 
-	// ICE (STUN/TURN) серверы
 	ice_servers: [
 		"stun:stun.l.google.com:19302",
 		"stun:stun1.l.google.com:19302",
@@ -32,10 +27,8 @@ module.exports = {
 		// }
 	],
 
-	// Максимум подключений
 	max_clients: 300,
 
-	// Настройки ожидания (таймаутов)
 	ping_frequency: 200000,
 	client_timeout: 500000,
 	inactive_timeout: 1200000,
@@ -43,10 +36,8 @@ module.exports = {
 	host_max_unconfirmed: 10,
 	flood_limit: 1000,
 
-	// Ограничения по длине названий
 	max_alias_length: 20,
 	max_name_length: 30,
 
-	// Нет ограничения по пирами на игру
 	max_game_peers: 0
 };
